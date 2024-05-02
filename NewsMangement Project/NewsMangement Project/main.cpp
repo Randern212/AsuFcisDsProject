1beb279b8fb3894f9efe8eaf84b1ac3559b9ef7c
Randern212 <128811970+Randern212@users.noreply.github.com>
2024-05-02 20:23:54 +0300

    Update main.cpp

--- NewsMangement Project/NewsMangement Project/main.cpp
+++ NewsMangement Project/NewsMangement Project/main.cpp
@@ -5,11 +5,34 @@
 #include "RegisteredUser.h"
 #include<set>
 #include<unordered_map>
-using namespace std;
-
+#include<assert.h>
+#include<fstream>
 int main()
 {
-	unordered_map<string, unordered_map<string, RegisteredUser>> userList;
+	int input;
 	User* currentUser;
+	std::unordered_map<string, User> accounts; //the key is 
+	std::cout << "To log-in press 1 to register press 2 \n";
+	std::cin >> input;
+	if (input == 1)
+	{
+		string name, password;
+		std::cout << "username: \t";
+		std::getline(std::cin,name);
+		std::cout << "password: \t";
+		std::getline(std::cin, password);
+		currentUser = accounts[name].Login(name,password);
+		if (currentUser != NULL)
+		{
 
+		}
+	}
+	else if(input==2)
+	{
+		string name, password;
+		std::cout << "username: \t";
+		std::getline(std::cin, name);
+		std::cout << "password: \t";
+		accounts.insert(std::make_pair(password, RegisteredUser(name, password)));
+	}
 }
