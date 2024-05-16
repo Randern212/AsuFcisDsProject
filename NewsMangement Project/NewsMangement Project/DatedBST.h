aaa8c017132047b7958b35be7f086707dc23ea05
Randern212 <128811970+Randern212@users.noreply.github.com>
2024-05-16 20:49:20 +0300

    Deleted old Folder to upload final version

--- NewsMangement Project/NewsMangement Project/DatedBST.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include<set>
-template <class T>
-class DatedBST :std::set<T> //Sara
-{
-};
-
--- NewsMangement Project/NewsMangement Project/ratedBST.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include<set>
-template <class T>
-class ratedBST : std::set<T> //Rokaya
-{
-
-};
